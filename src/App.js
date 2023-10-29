82cda08f459ba97db31913fa03959b0d08d399d6
DASaBeR <m.saberi1999@gmail.com>
2023-10-29 12:20:37 +0100

    add some component and add styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
+import "./App.css";
 
 function App() {
+  const expenses = [
+    { title: "Buy Sandwich", amount: 199, date: new Date(2023, 10, 23) },
+    { title: "Car Insurace", amount: 980, date: new Date(2023, 10, 21) },
+    { title: "Snapp", amount: 20, date: new Date(2023, 10, 12) },
+    { title: "Buy Internet", amount: 15, date: new Date(2023, 10, 15) },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Expenses items={expenses} />
     </div>
   );
 }
